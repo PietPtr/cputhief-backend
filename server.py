8265f8d33a8db37e98f86359be0b3b435b420cd0
Pieter Staal <pieterstaal@outlook.com>
2017-11-29 15:04:07 +0100

    Add block validation

--- server.py
+++ server.py
@@ -1,8 +1,23 @@
 from flask import Flask
 from flask import request
 import api
+import json
+import threading
+import validation
+import os
 
 app = Flask(__name__)
+
+block_size = 1024
+
+pointer = int(os.popen("ls /var/www/html/primes | tail -n 1").read().split('_')[1])
+print("starting at", pointer, "...")
+
+primeFile = open("primes.dat", "wb")
+
+val_thread = threading.Thread(target=validation.validation_loop)
+val_thread.deamon = True
+val_thread.start()
 
 @app.route("/api/test", methods=['POST', 'GET'])
 def test():
@@ -10,5 +25,32 @@ def test():
 
 @app.route("/api/get_number", methods=['GET'])
 def get_number():
-    block = { "block": 0 }
+    global pointer
+    global block_size
+
+    if len(validation.missing) > 0:
+        print(validation.missing)
+        block = { "block": validation.missing[0], "n": block_size }
+        validation.missing.pop(0)
+        return api.send_json(block)
+
+    block = { "block": pointer, "n": block_size }
+    pointer += block_size
     return api.send_json(block)
+
+@app.route("/api/post_results", methods = ['POST'])
+def post_results():
+    data = json.loads(request.data.decode())
+
+    primeFile = open("/var/www/html/primes/block_" + str(data["block"]) + \
+                     "_" + str(block_size) + ".txt", "w")
+
+    for prime in data["result"]:
+        primeFile.write(str(prime) + "\n")
+
+    return api.success()
+
+@app.route("/api/num_primes", methods = ['GET'])
+def num_primes():
+    amount = 0
+    return api.succes()
---
+++ validation.py
@@ -0,0 +1,47 @@
+import os
+import time
+
+missing = []
+
+"""
+Checks if there are any missing blocks in the primes directory, and returns a
+list of the missing block numbers.
+    start: the block from which the function should validate
+"""
+def validate(start):
+    print("Validating all blocks from", start)
+    names = os.popen("ls /var/www/html/primes | sort -V").read().strip().split("\n")
+
+    blocks = []
+
+    for name in names:
+        try:
+            block = int(name.split('_')[1])
+            block_size = int(name.split('_')[2].replace(".txt", ''))
+        except IndexError:
+            print("Unrecognized filename, continueing...")
+            continue
+
+        blocks.append([block, block_size])
+
+
+    i = 0
+    for block in blocks:
+        if i < len(blocks) - 1:
+            if blocks[i + 1][0] != block[0] + block[1]:
+                print("miss!", block, blocks[i + 1])
+                check_block_num = block[0] + block[1]
+                # zolang we nog niet bij het goede block zijn...
+                while check_block_num != blocks[i + 1][0]:
+                    missing.append(check_block_num)
+                    check_block_num += block[1]
+
+        i += 1
+
+    return missing
+
+def validation_loop():
+    while True:
+        # validate everything every hour
+        validate(1600000000000000)
+        time.sleep(60 * 60)
